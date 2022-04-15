2f07d59f44aa81a9f003eb554dcdfe921612f115
JvMapote <jayveenmapote@gmail.com>
2022-04-15 19:46:15 +0800

    commit

--- IntermidiateCourse/IntermidiateCourse.py
+++ IntermidiateCourse/IntermidiateCourse.py
@@ -1,8 +1,11 @@
-class Guest:
-    def __init__(self, first, last, interest, phone):
-        self.first = first
-        self.last = last
-        self.interest = interest
-        self.phone = phone
+def usingInit():
+    class Guest:
+        def __init__(self, first, last, interest, phone):
+            self.first = first
+            self.last = last
+            self.interest = interest
+            self.phone = phone
 
-g_1 = Guest("Jayvee", "Mapote", "Anime", 2020161610)
+    g_1 = Guest("Jayvee", "Mapote", "Anime", 2020161610)
+    print(g_1.first)
+usingInit()
